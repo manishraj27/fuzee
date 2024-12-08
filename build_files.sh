af10c1f193a5f97acfed033269895baefd223549
manishraj27 <manish012raj@gmail.com>
2024-12-08 20:39:34 +0530

    Enhance build process by upgrading pip, installing dependencies, and configuring Vercel settings

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,6 @@
-pip install -r requirements.txt
+# Install python dependencies
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+
+# Collect static files
 python3 manage.py collectstatic --noinput
